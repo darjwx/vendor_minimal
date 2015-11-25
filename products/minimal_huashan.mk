7a231ade91969214c80600cbaae7b306975c17b8
darjwx <darjwx@gmail.com>
2015-11-25 15:59:54 +0100

    fix build

--- products/minimal_huashan.mk
+++ products/minimal_huashan.mk
@@ -26,9 +26,8 @@ PRODUCT_MANUFACTURER := Sony
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=huashan \
     BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.1.207/Nvt_nw:user/release-keys \
-    PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 \
+    PRODUCT_NAME=C5303 \
     PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-140318-1014 227 test-keys"
     
 
